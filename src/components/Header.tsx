9785046e1e42a001b8afa6fb118eab5d969df0d9
Mohamed Rifaath <rifkrifaath@gmail.com>
2026-07-24 19:42:04 +0400

    Remove unnecessary files and add header

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header className="flex items-center justify-between px-16 py-6 shadow-md">
+      <h1 className="text-2xl font-extrabold">Where in the world?</h1>
+      <button type="button">Dark Mode</button>
+    </header>
+  )
+}
+
+export default Header
